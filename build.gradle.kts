36d265d982320e7000895fd109e12591cc897356
konstantin <mailspace.hub@gmail.com>
2025-03-05 13:47:11 +0300

    Добавлена зависимость и конфиги для prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
 	compileOnly("org.projectlombok:lombok")
 
